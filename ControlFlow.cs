17faeab57b787b15f9278764281b135bfc198692
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-19 07:52:37 +0000

    Adding control flow ops

---
+++ ControlFlow.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Emit;
+
+namespace ILGeneratorExtensions
+{
+    public static class ControlFlow
+    {
+        public static void BranchTo(this ILGenerator generator, Label label) => generator.Emit(OpCodes.Br, label);
+
+        public static void BranchToShortForm(this ILGenerator generator, Label label) => generator.Emit(OpCodes.Br_S, label);
+
+        public static void Switch(this ILGenerator generator, params Label[] labels) => generator.Emit(OpCodes.Switch, labels);
+    }
+}
