d606b0fb89b8d9df1cdb5b22952b20f5f2be6f68
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-19 07:57:21 +0000

    Add debug ops

---
+++ DebugOperations.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Emit;
+
+namespace ILGeneratorExtensions
+{
+    public static class DebugOperations
+    {
+        public static void NoOp(this ILGenerator generator) => generator.Emit(OpCodes.Nop);
+        public static void NoOperation(this ILGenerator generator) => generator.NoOp();
+
+        public static void Break(this ILGenerator generator) => generator.Emit(OpCodes.Break);
+        public static void BreakInDebugger(this ILGenerator generator) => generator.Break();
+
+    }
+}
