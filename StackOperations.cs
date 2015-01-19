9424bf1bdd794a6004c69873b0ed3172e067b125
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-19 04:55:23 +0000

    Add stack operations

---
+++ StackOperations.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.Emit;
+
+namespace ILGeneratorExtensions
+{
+    public static class StackOperations
+    {
+        public static void Pop(this ILGenerator generator) => generator.Emit(OpCodes.Pop);
+
+        public static void Pop(this ILGenerator generator, uint repeat)
+        {
+            for (int i = 0; i < repeat; i++)
+            {
+                generator.Emit(OpCodes.Pop);
+            }
+        }
+
+        public static void Duplicate(this ILGenerator generator) => generator.Emit(OpCodes.Dup);
+
+        public static void Duplicate(this ILGenerator generator, uint repeat)
+        {
+            for (int i = 0; i < repeat; i++)
+            {
+                generator.Emit(OpCodes.Dup);
+            }
+        }
+    }
+}
