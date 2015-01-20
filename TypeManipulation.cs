17b639bf183d762abc521688a2b56b72fe869fc8
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-20 16:59:03 +0000

    More type manipulation ops

--- TypeManipulation.cs
+++ TypeManipulation.cs
@@ -8,5 +8,45 @@ namespace ILGeneratorExtensions
         public static void IsInstanceOfType<T>(this ILGenerator generator) => generator.IsInstanceOfType(typeof(T));
 
         public static void IsInstanceOfType(this ILGenerator generator, Type type) => generator.Emit(OpCodes.Isinst, type);
+
+        public static void CastClass(this ILGenerator generator, Type type)
+        {
+            if (type.IsValueType)
+            {
+                throw new InvalidOperationException("Cannot cast to a value type");
+            }
+
+            generator.Emit(OpCodes.Castclass, type);
+        }
+
+        public static void CastClass<T>(this ILGenerator generator) where T : class => generator.CastClass(typeof (T));
+
+        public static void Box(this ILGenerator generator, Type type)
+        {
+            if (!type.IsValueType)
+            {
+                throw new InvalidOperationException("Can only box value types");
+            }
+
+            generator.Emit(OpCodes.Box, type);
+        }
+
+        public static void Box<T>(this ILGenerator generator) where T : struct => generator.Box(typeof (T));
+
+        public static void Unbox(this ILGenerator generator, Type type)
+        {
+            if (!type.IsValueType)
+            {
+                throw new InvalidOperationException("Cannot unbox non-value types");
+            }
+
+            generator.Emit(OpCodes.Unbox, type);
+        }
+
+        public static void Unbox<T>(this ILGenerator generator) where T : struct => generator.Unbox(typeof (T));
+
+        public static void UnboxAny(this ILGenerator generator, Type type) => generator.Emit(OpCodes.Unbox_Any, type);
+
+        public static void UnboxAny<T>(this ILGenerator generator) => generator.UnboxAny(typeof (T));
     }
 }
