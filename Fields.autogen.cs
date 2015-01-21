df27f6e9e0a79868cf23f53366f3e92511f57e75
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-21 15:35:16 +0000

    Field handling opcodes

---
+++ Fields.autogen.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ILGeneratorExtensions
+{
+    public static partial class Fields
+    {
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, Boolean value)
+		{
+			if (field.FieldType != typeof(Boolean))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, Boolean value)
+		{
+			if (field.FieldType != typeof(Boolean))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, Char value)
+		{
+			if (field.FieldType != typeof(Char))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, Char value)
+		{
+			if (field.FieldType != typeof(Char))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, SByte value)
+		{
+			if (field.FieldType != typeof(SByte))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, SByte value)
+		{
+			if (field.FieldType != typeof(SByte))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, Byte value)
+		{
+			if (field.FieldType != typeof(Byte))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, Byte value)
+		{
+			if (field.FieldType != typeof(Byte))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, Int16 value)
+		{
+			if (field.FieldType != typeof(Int16))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, Int16 value)
+		{
+			if (field.FieldType != typeof(Int16))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, UInt16 value)
+		{
+			if (field.FieldType != typeof(UInt16))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, UInt16 value)
+		{
+			if (field.FieldType != typeof(UInt16))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, Int32 value)
+		{
+			if (field.FieldType != typeof(Int32))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, Int32 value)
+		{
+			if (field.FieldType != typeof(Int32))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, UInt32 value)
+		{
+			if (field.FieldType != typeof(UInt32))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, UInt32 value)
+		{
+			if (field.FieldType != typeof(UInt32))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, Int64 value)
+		{
+			if (field.FieldType != typeof(Int64))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, Int64 value)
+		{
+			if (field.FieldType != typeof(Int64))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, UInt64 value)
+		{
+			if (field.FieldType != typeof(UInt64))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, UInt64 value)
+		{
+			if (field.FieldType != typeof(UInt64))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, Single value)
+		{
+			if (field.FieldType != typeof(Single))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, Single value)
+		{
+			if (field.FieldType != typeof(Single))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+		public static void OverwriteFieldWith(this ILGenerator generator, FieldInfo field, Double value)
+		{
+			if (field.FieldType != typeof(Double))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.StoreInField(field);
+		}
+
+		public static void OverwriteFieldWithVolatile(this ILGenerator generator, FieldInfo field, Double value)
+		{
+			if (field.FieldType != typeof(Double))
+			{
+				throw new InvalidOperationException("Type mismatch - field is of type " + field.FieldType);
+			}
+
+			generator.LoadConstant(value);
+			generator.Emit(OpCodes.Volatile);
+			generator.StoreInField(field);
+		}
+	}
+}
---
+++ Fields.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ILGeneratorExtensions
+{
+    public static partial class Fields
+    {
+        public static void LoadField(this ILGenerator generator, FieldInfo field)
+            => generator.Emit(field.IsStatic ? OpCodes.Ldsfld : OpCodes.Ldfld, field);
+
+        public static void LoadFieldVolatile(this ILGenerator generator, FieldInfo field)
+        {
+            generator.Emit(OpCodes.Volatile);
+            generator.LoadField(field);
+        }
+
+        public static void LoadFieldAddress(this ILGenerator generator, FieldInfo field)
+        {
+            generator.Emit(field.IsStatic ? OpCodes.Ldsflda : OpCodes.Ldflda, field);
+        }
+
+        public static void LoadFieldAddressVolatile(this ILGenerator generator, FieldInfo field)
+        {
+            generator.Emit(OpCodes.Volatile);
+            generator.LoadFieldAddress(field);
+        }
+
+        public static void StoreInField(this ILGenerator generator, FieldInfo field)
+            => generator.Emit(field.IsStatic ? OpCodes.Stsfld : OpCodes.Stfld, field);
+
+        public static void StoreInFieldVolatile(this ILGenerator generator, FieldInfo field)
+        {
+            generator.Emit(OpCodes.Volatile);
+            generator.StoreInField(field);
+        }
+    }
+}
