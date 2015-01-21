e616505d8fce8e7a7f132e61aad51d7442f3d16c
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-21 15:06:10 +0000

    Function pointers

---
+++ FunctionPointers.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ILGeneratorExtensions
+{
+    public static class FunctionPointers
+    {
+        public static void LoadFunctionPointer(this ILGenerator generator, MethodInfo method)
+            => generator.Emit(OpCodes.Ldftn, method);
+
+        public static void LoadVirtualFunctionPointer(this ILGenerator generator, MethodInfo method)
+            => generator.Emit(OpCodes.Ldvirtftn, method);
+    }
+}
