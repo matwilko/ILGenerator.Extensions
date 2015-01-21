b243cd128651f360567104aeda7cfcaec0e23c0a
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-21 15:02:06 +0000

    Typed references ops

---
+++ TypedReferences.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace ILGeneratorExtensions
+{
+    public static class TypedReferences
+    {
+        public static void MakeTypedReference(this ILGenerator generator, Type type) => generator.Emit(OpCodes.Mkrefany, type);
+
+        public static void MakeTypedReference<T>(this ILGenerator generator) => generator.MakeTypedReference(typeof (T));
+
+        public static void GetTypeOfTypedReference(this ILGenerator generator) => generator.Emit(OpCodes.Refanytype);
+
+        public static void GetAddressOfTypedReference(this ILGenerator generator, Type type) => generator.Emit(OpCodes.Refanyval, type);
+
+        public static void GetAddressOfTypedReference<T>(this ILGenerator generator) => generator.GetAddressOfTypedReference(typeof (T));
+    }
+}
