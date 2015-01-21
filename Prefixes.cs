f0cadfc522746e8927fa316c6c0543db12984cc6
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-21 16:51:02 +0000

    unaligned prefix

---
+++ Prefixes.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection.Emit;
+
+namespace ILGeneratorExtensions
+{
+    public static class Prefixes
+    {
+        public static void Unaligned(this ILGenerator generator) => generator.Emit(OpCodes.Unaligned);
+    }
+}
