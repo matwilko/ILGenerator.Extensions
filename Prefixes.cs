5f0f631e9c831e28e24f6eaf4475273db4fda3fa
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-27 20:18:08 +0000

    Prefix + StackOperations docs

--- Prefixes.cs
+++ Prefixes.cs
@@ -6,6 +6,10 @@ namespace ILGeneratorExtensions
     [PublicAPI]
     public static class Prefixes
     {
+        /// <summary>
+        /// Indicates that the next operation should not assume that its arguments are properly aligned
+        /// </summary>
+        /// <param name="generator"></param>
         [PublicAPI]
         public static void Unaligned(this ILGenerator generator) => generator.Emit(OpCodes.Unaligned);
     }
--- StackOperations.cs
+++ StackOperations.cs
@@ -6,25 +6,43 @@ namespace ILGeneratorExtensions
     [PublicAPI]
     public static class StackOperations
     {
+        /// <summary>
+        /// Pops the top value off the execution stack and discards it
+        /// </summary>
+        /// <param name="generator"></param>
         [PublicAPI]
         public static void Pop(this ILGenerator generator) => generator.Emit(OpCodes.Pop);
 
+        /// <summary>
+        /// Pops <paramref name="n"/> values off the execution stack and discards them
+        /// </summary>
+        /// <param name="generator"></param>
+        /// <param name="n">The number of stack values to discard</param>
         [PublicAPI]
-        public static void Pop(this ILGenerator generator, uint repeat)
+        public static void Pop(this ILGenerator generator, uint n)
         {
-            for (int i = 0; i < repeat; i++)
+            for (int i = 0; i < n; i++)
             {
                 generator.Emit(OpCodes.Pop);
             }
         }
 
+        /// <summary>
+        /// Duplicates the value on the top of the stack
+        /// </summary>
+        /// <param name="generator"></param>
         [PublicAPI]
         public static void Duplicate(this ILGenerator generator) => generator.Emit(OpCodes.Dup);
 
+        /// <summary>
+        /// Duplicates the value on the top of the execution stack <paramref name="n"/> times
+        /// </summary>
+        /// <param name="generator"></param>
+        /// <param name="n">The number of times to duplicate the value</param>
         [PublicAPI]
-        public static void Duplicate(this ILGenerator generator, uint repeat)
+        public static void Duplicate(this ILGenerator generator, uint n)
         {
-            for (int i = 0; i < repeat; i++)
+            for (int i = 0; i < n; i++)
             {
                 generator.Emit(OpCodes.Dup);
             }
