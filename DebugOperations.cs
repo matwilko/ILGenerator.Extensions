9622e0307d006b971406578cb2aef4cfcff2f3a6
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-27 19:08:20 +0000

    DebugOperations documentation

--- DebugOperations.cs
+++ DebugOperations.cs
@@ -6,15 +6,31 @@ namespace ILGeneratorExtensions
     [PublicAPI]
     public static class DebugOperations
     {
+        /// <summary>
+        /// Performs no operation
+        /// </summary>
+        /// <param name="generator"></param>
         [PublicAPI]
         public static void NoOp(this ILGenerator generator) => generator.Emit(OpCodes.Nop);
 
+        /// <summary>
+        /// Performs no operation
+        /// </summary>
+        /// <param name="generator"></param>
         [PublicAPI]
         public static void NoOperation(this ILGenerator generator) => generator.NoOp();
 
+        /// <summary>
+        /// Signals an attached debugger to break execution
+        /// </summary>
+        /// <param name="generator"></param>
         [PublicAPI]
         public static void Break(this ILGenerator generator) => generator.Emit(OpCodes.Break);
 
+        /// <summary>
+        /// Signals an attached debugger to break execution
+        /// </summary>
+        /// <param name="generator"></param>
         [PublicAPI]
         public static void BreakInDebugger(this ILGenerator generator) => generator.Break();
     }
