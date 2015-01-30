f4ad83ec877a794e21e29f75707a8ac8a5df6140
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-01-30 18:35:29 +0000

    TypedReferences + Tokens docs

--- Tokens.cs
+++ Tokens.cs
@@ -8,14 +8,29 @@ namespace ILGeneratorExtensions
     [PublicAPI]
     public static class Tokens
     {
+        /// <summary>
+        /// Pushes the token for the given type onto the execution stack
+        /// </summary>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
+        /// <param name="type">The type to load the token of</param>
         [PublicAPI]
         public static void LoadTokenFor(this ILGenerator generator, Type type) => generator.Emit(OpCodes.Ldtoken, type);
 
+        /// <summary>
+        /// Pushes the token for the given type onto the execution stack
+        /// </summary>
+        /// <typeparam name="T">The type to load the token of</typeparam>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
         [PublicAPI]
         public static void LoadTokenFor<T>(this ILGenerator generator) => generator.Emit(OpCodes.Ldtoken, typeof (T));
 
         private static readonly MethodInfo GetTypeFromHandle = typeof (Type).GetMethod("GetTypeFromHandle");
 
+        /// <summary>
+        /// Pushes the <see cref="Type" /> of the given type onto the execution stack
+        /// </summary>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
+        /// <param name="type">The type to load the <see cref="Type" /> of</param>
         [PublicAPI]
         public static void TypeOf(this ILGenerator generator, Type type)
         {
@@ -23,14 +38,29 @@ namespace ILGeneratorExtensions
             generator.Call(GetTypeFromHandle);
         }
 
+        /// <summary>
+        /// Pushes the <see cref="Type" /> of the given type onto the execution stack
+        /// </summary>
+        /// <typeparam name="T">The type to load the <see cref="Type" /> of</typeparam>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
         [PublicAPI]
         public static void TypeOf<T>(this ILGenerator generator) => generator.TypeOf(typeof (T));
 
+        /// <summary>
+        /// Pushes the token for the given method onto the execution stack
+        /// </summary>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
+        /// <param name="method">The method to load the token of</param>
         [PublicAPI]
         public static void LoadTokenFor(this ILGenerator generator, MethodInfo method) => generator.Emit(OpCodes.Ldtoken, method);
         
         private static readonly MethodInfo GetMethodFromHandle = typeof(MethodBase).GetMethod("GetMethodFromHandle", new [] { typeof(RuntimeMethodHandle) });
 
+        /// <summary>
+        /// Pushes the <see cref="MethodInfo" /> of the given method onto the execution stack
+        /// </summary>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
+        /// <param name="method">The method to load the <see cref="MethodInfo" /> of</param>
         [PublicAPI]
         public static void MethodInfoFor(this ILGenerator generator, MethodInfo method)
         {
@@ -38,11 +68,21 @@ namespace ILGeneratorExtensions
             generator.Call(GetMethodFromHandle);
         }
 
+        /// <summary>
+        /// Pushes the token for the given field onto the execution stack
+        /// </summary>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
+        /// <param name="field">The field to load the token of</param>
         [PublicAPI]
         public static void LoadTokenFor(this ILGenerator generator, FieldInfo field) => generator.Emit(OpCodes.Ldtoken, field);
 
         private static readonly MethodInfo GetFieldFromHandle = typeof (FieldInfo).GetMethod("GetFieldFromHandle", new [] { typeof (RuntimeFieldHandle) });
 
+        /// <summary>
+        /// Pushes the <see cref="FieldInfo" /> of the given field onto the execution stack
+        /// </summary>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
+        /// <param name="field">The field to load the <see cref="FieldInfo" /> of</param>
         [PublicAPI]
         public static void FieldInfoFor(this ILGenerator generator, FieldInfo field)
         {
--- TypedReferences.cs
+++ TypedReferences.cs
@@ -7,18 +7,42 @@ namespace ILGeneratorExtensions
     [PublicAPI]
     public static class TypedReferences
     {
+        /// <summary>
+        /// Pops a pointer to a piece of data off the execution stack and pushes a typed reference (of the given type)
+        /// </summary>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
+        /// <param name="type">The type of the resulting reference</param>
         [PublicAPI]
         public static void MakeTypedReference(this ILGenerator generator, Type type) => generator.Emit(OpCodes.Mkrefany, type);
 
+        /// <summary>
+        /// Pops a pointer to a piece of data off the execution stack and pushes a typed reference (of the given type)
+        /// </summary>
+        /// <typeparam name="T">The type of the resulting reference</typeparam>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
         [PublicAPI]
         public static void MakeTypedReference<T>(this ILGenerator generator) => generator.MakeTypedReference(typeof (T));
 
+        /// <summary>
+        /// Pops a typed reference from the execution stack and pushes the type token of its type
+        /// </summary>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
         [PublicAPI]
         public static void GetTypeOfTypedReference(this ILGenerator generator) => generator.Emit(OpCodes.Refanytype);
 
+        /// <summary>
+        /// Pops a typed reference (of the given type) from the execution stack and pushes the address of the reference
+        /// </summary>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
+        /// <param name="type">The type of the reference</param>
         [PublicAPI]
         public static void GetAddressOfTypedReference(this ILGenerator generator, Type type) => generator.Emit(OpCodes.Refanyval, type);
 
+        /// <summary>
+        /// Pops a typed reference (of the given type) from the execution stack and pushes the address of the reference
+        /// </summary>
+        /// <typeparam name="T">The type of the reference</typeparam>
+        /// <param name="generator">The <see cref="T:System.Reflection.Emit.ILGenerator" /> to emit instructions from</param>
         [PublicAPI]
         public static void GetAddressOfTypedReference<T>(this ILGenerator generator) => generator.GetAddressOfTypedReference(typeof (T));
     }
