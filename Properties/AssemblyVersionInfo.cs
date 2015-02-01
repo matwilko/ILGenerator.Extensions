dc34794aef818e15260f7a044521e1fedb331f45
Matt Wilkinson <matthew.wilkinson@gmail.com>
2015-02-01 05:14:28 +0000

    Version bump

--- Properties/AssemblyVersionInfo.cs
+++ Properties/AssemblyVersionInfo.cs
@@ -15,5 +15,5 @@ Copyright 2014 Matthew Wilkinson
 */
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.0.12")]
-[assembly: AssemblyFileVersion("1.1.0.12")]
+[assembly: AssemblyVersion("1.2.0.13")]
+[assembly: AssemblyFileVersion("1.2.0.13")]
